236a5739dc883d37274772c2b4c78610aedfcda4
vanchung1995 <vanchung1995@gmail.com>
2024-07-25 20:17:46 +0700

    add route filteredimage

--- project starter code/server.js
+++ project starter code/server.js
@@ -30,6 +30,39 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
     /**************************************************************************** */
 
   //! END @TODO1
+  app.get( "/filteredimage", async (req, res) => {
+    const image_url = req.query.image_url;
+ 
+    if (!image_url) {
+      const current_path = req.get('host') + req.path;
+      res.send(`You should include image_url param on query:    <code><b>${current_path}?image_url={{URL}}</b></code><br>
+        This image_url need to be encoded`)
+      return;
+    }
+ 
+    const validUrl = URL.canParse(image_url);
+    if (!validUrl) {
+      res.send(`image_url param should be a valid url and need to be encoded`)
+      return;
+    }
+ 
+    try {
+      const outpath = await filterImageFromURL(image_url);
+      console.log('image_url: ' + image_url);
+      console.log("output path: ", outpath)
+ 
+      res.status(200).sendFile(outpath, err => {
+        if (err) {
+            console.log("Cannot send the file to client. Error: ",err);
+            res.sendStatus(500);
+        }
+        deleteLocalFiles([outpath]);
+     });
+    } catch (error) {
+      console.log("Error: ", error)
+      res.status(404).send(`Cannot get image from url`)
+    }
+  });
   
   // Root Endpoint
   // Displays a simple message to the user
